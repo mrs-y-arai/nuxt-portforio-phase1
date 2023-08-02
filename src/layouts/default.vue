5e7d2d2920b2e7d247bd1c406d5534bacb2e15cd
mrs-y-arai <sidvivid9672@gmail.com>
2023-08-02 12:10:03 +0900

    💄 アップデート

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-strongBlack">
+  <div class="overflow-x-hidden">
     <VHeader />
     <main class="w-full px-8">
       <slot />
@@ -7,14 +7,3 @@
     <VFooter />
   </div>
 </template>
-<script setup lang="ts">
-  const route = useRoute();
-  const pagePath = computed(() => route.path);
-</script>
-<style>
-  body {
-    font-family: "Helvetica", "Noto Sans JP", sans-serif;
-    color: #fff;
-    overflow-x: hidden;
-  }
-</style>
